8c71be58b631f98ce8985b23f8343e46bcce33a8
Samay42 <samaypatel443@gmail.com>
2025-03-14 01:09:27 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 # Allow frontend to communicate with backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://map-mind.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
